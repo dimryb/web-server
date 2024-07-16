f96c644a2173c02ffd00a2c869991407fb4adfe5
dimryb <dimryb@bk.ru>
2024-07-16 20:17:44 +0700

    Стандартный роутер

--- 01_hello/main.go
+++ 01_hello/main.go
@@ -6,28 +6,29 @@ import (
 	"strings"
 )
 
-type MyHandler struct {
-}
-
-func (MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		w.Write([]byte("Home"))
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/hello/") {
-		name := strings.Split(r.URL.Path, "/")[2]
-		w.Write([]byte(fmt.Sprintf("Hello, %s", name)))
-		return
-	}
-
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte("404 Page Not Found"))
-}
-
 func main() {
-	err := http.ListenAndServe(":3000", MyHandler{})
+	http.HandleFunc("/", index)
+	http.HandleFunc("/hello/", hello)
+
+	http.Handle("/foo", foo())
+
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func foo() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Foo"))
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Home"))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	name := strings.Split(r.URL.Path, "/")[2]
+	w.Write([]byte(fmt.Sprintf("Hello, %s", name)))
 }
