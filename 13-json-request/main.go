68efb0f559b77e120b78663d3bec525e580c6f50
dimryb <dimryb@bk.ru>
2024-08-01 16:27:40 +0700

    Парсинг json из запроса

---
+++ 13-json-request/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	http.HandleFunc("/user", UserHandler)
+	http.ListenAndServe(":3000", nil)
+}
+
+func WriteJson(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		WriteJson(w, http.StatusMethodNotAllowed, map[string]any{
+			"ok":    false,
+			"error": "method not allowed",
+		})
+	}
+
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		WriteJson(w, http.StatusInternalServerError, map[string]any{
+			"ok":    false,
+			"error": err.Error(),
+		})
+		return
+	}
+
+	fmt.Printf("user %v", user)
+	WriteJson(w, http.StatusOK, map[string]any{
+		"ok": true,
+
+	})
+}
