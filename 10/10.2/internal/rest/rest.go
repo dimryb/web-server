071995e8773cb5142bef0d0959e8d28dd325ee95
dimryb <dimryb@bk.ru>
2024-09-04 21:23:11 +0700

    Добавил модуль rest

---
+++ 10/10.2/internal/rest/rest.go
@@ -0,0 +1,25 @@
+package rest
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Ok     bool   `json:"ok"`
+	Result any    `json:"result,omitemmpty"`
+	Error  string `json:"json:error,omitempty"`
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) {
+	bytes, _ := json.Marshal(v)
+	w.WriteHeader(status)
+	w.Write(bytes)
+}
+
+func WriteError(w http.ResponseWriter, status int, err error) {
+	WriteJSON(w, status, Response{
+		Ok:    false,
+		Error: err.Error(),
+	})
+}
